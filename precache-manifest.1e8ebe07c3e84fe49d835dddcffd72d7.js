47b27e97285a9fe586376e5649f7c2100ae28ee2
GAliNor <ali.atguedda@gmail.com>
2020-08-08 21:54:10 +0200

    Updates

---
+++ precache-manifest.1e8ebe07c3e84fe49d835dddcffd72d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647e7c70a8e93f55cca5c92a6ef15853",
+    "url": "/monster-load/index.html"
+  },
+  {
+    "revision": "d1b6bfdb36c125009391",
+    "url": "/monster-load/static/css/main.25e40710.chunk.css"
+  },
+  {
+    "revision": "c9edde8843441b222e1a",
+    "url": "/monster-load/static/js/2.4e4aba6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-load/static/js/2.4e4aba6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b6bfdb36c125009391",
+    "url": "/monster-load/static/js/main.e9577ef4.chunk.js"
+  },
+  {
+    "revision": "95f23f794a388448234f",
+    "url": "/monster-load/static/js/runtime-main.dc8a2d29.js"
+  }
+]);
